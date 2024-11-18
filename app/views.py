71c6915249c6fb15571c87452f717d074a1102d7
LucasIbarra03 <lucasgibarra2003@gmail.com>
2024-11-18 16:51:16 -0300

    Update views.py
    Aca complete la funcion def home

--- app/views.py
+++ app/views.py
@@ -4,18 +4,24 @@ from django.shortcuts import redirect, render
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-
+from .layers.services import *
+from .models import Favourite
 def index_page(request):
     return render(request, 'index.html')
 
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
-    images = []
-    favourite_list = []
+    input_value = request.GET.get('search', None)    
+    characters_list = services.getAllImages(input=input_value)  # Corrige la llamada al método
 
-    return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
+    # Asigna characters_list a la variable images que será utilizada en el render
+    images = characters_list
 
+    # Define favourite_list para evitar errores si no está definido
+    favourite_list = []  # Reemplaza con tu lógica para obtener la lista de favoritos
+
+    return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list })
 def search(request):
     search_msg = request.POST.get('query', '')
 
